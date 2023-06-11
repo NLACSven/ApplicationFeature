168ac1e074a1cf47feb27e877333a48d825cef95
Sven Lieball <svenlieball@gmail.com>
2023-06-11 14:45:38 +0200

    MAUI Application created

---
+++ ApplicationFeature/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ApplicationFeature;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ApplicationFeature/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ApplicationFeature;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
